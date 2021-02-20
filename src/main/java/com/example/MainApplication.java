e83984b22df3c40264d2b9d1dbd96eafd8940ecb
JacobCN <coder211@163.com>
2021-02-20 10:45:11 +0800

    feat: init spring boot project

--- src/main/java/com/example/MainApplication.java
+++ src/main/java/com/example/MainApplication.java
@@ -1,13 +1,18 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+        System.out.println("SpringVersion: " + SpringVersion.getVersion());
+        System.out.println("SpringBootVersion: " + SpringBootVersion.getVersion());
+        System.getProperties().list(System.out);
     }
 
 }
